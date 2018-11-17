9c54064a387947cb880ea7ef52027efb8ea68721
Grawor <41929192+Grawor@users.noreply.github.com>
2018-11-18 01:37:36 +0900

    a

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 #!/usr/bin/python3
@@ -37,7 +37,7 @@ class Common():
         else:
             print(file_name + " が存在しません。")
  
-    def get_csv_val_by_key(self, file_name, key):
+    def get_csv_val_by_key(self, file_name, name):
         """
         @brief : ファイル名とkeyを指定してCSVの値を取得
         @note  : ファイルに格納されているデータは下記のような形式
@@ -52,54 +52,57 @@ class Common():
             for row in reader:
                 key_dict[row[0]] = row[1]
                 #print(key_dict)
-        return key_dict[key]
+        return key_dict[name]
 
     # ----- LINE, DISCORD, SLACK にメッセージを送るための初期設定 関連----
-    def set_line(token):
+    def set_line(self, token):
         self.line_notify_token = token
 
-    def set_discord(url, username):
+    def set_discord(self, url, username):
         self.discord_webhook_url = url
         self.discord_username = username
     
-    def set_slack(url, username):
+    def set_slack(self, url, username):
         self.slack_webhook_url = url
         self.slack_username = username        
 
     # ----- Notify（メッセージを送る） 関連-----
-    def send_message(text) :
+    def send_message(self, text) :
         try:
-            __line(text)
+            self.__line(text)
         except:
             pass
         try:
-            __discord(text)
+            self.__discord(text)
         except:
             pass
         try:
-            __slack(text)
+            self.__slack(text)
         except:
             pass
         
-    def __line(message):
+    def __line(self, message):
         if len(self.line_notify_token) > 0:
             requests.post('https://notify-api.line.me/api/notify', headers={'Authorization': 'Bearer ' + self.line_notify_token}, data={'message': '\n' + message})
 
-    def __discord(message):
+    def __discord(self, message):
         if len(self.discord_webhook_url) > 0:
             requests.post(self.discord_webhook_url, data={'username': self.discord_username, 'content': message})
 
-    def __slack(message):
+    def __slack(self, message):
         if len(self.slack_webhook_url) > 0:
             requests.post(self.slack_webhook_url, data=json.dumps({'username': self.slack_username, 'text':message}))
 
     
 
 
-# In[12]:
+# In[2]:
 
 
+# ---- For Debug ----
 #common = Common()
-#key = common.get_csv_val("../keys.csv", 1,1)
-#print(key)
+#token = common.get_csv_val_by_key("../keys.csv", "line_token")
+#print(token)
+#common.set_line(token)
+#common.send_message("test")
 
